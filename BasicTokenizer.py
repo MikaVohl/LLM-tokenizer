65b01084c8a9be65532dd06e60a599fa4418d21c
Mika Vohl <mikavohl@gmail.com>
2025-05-13 11:38:44 -0400

    Added encoding ability

--- BasicTokenizer.py
+++ BasicTokenizer.py
@@ -2,7 +2,7 @@
 
 class BasicTokenizer():
     def __init__(self):
-        pass
+        self.merges = {}
 
     def get_stats(self, ids):
         counts = {}
@@ -24,30 +24,33 @@ class BasicTokenizer():
             
 
     def train(self, text, vocab_size, verbose=False):
-        num_merges = vocab_size - 256  # UTF-8 has 256 values
-        ids = text.encode("utf-8") # convert text to UTF-8 bytes
+        num_merges = max(0, vocab_size - 256)  # UTF-8 has 256 values
+        ids = list(text.encode("utf-8")) # convert text to list of integer representations of UTF-8 bytes
         # TODO: do the regex splitting that OpenAI does
-        merges = {}
+        self.merges = {}
         for i in range(num_merges):
             stats = self.get_stats(ids)
             pair = max(stats, key=stats.get)
             idx = 256 + i
             ids = self.merge(ids, pair, idx)
-            merges[pair] = idx
+            self.merges[pair] = idx
         return ids
             
 
     def encode(self, text):
-        pass
+        ids = list(text.encode("utf-8"))
+        for pair, idx in self.merges:
+            ids = self.merge(ids, pair, idx)
+        return ids
+
 
     def decode(self, ids):
         pass
 
 tok = BasicTokenizer()
 
-msg = "Train your tokenizer on whatever text you like and visualize the merged tokens. Do they look reasonable? One default test you may wish to use is the text file tests/taylorswift.txt."
-output = tok.train(msg, 300)
+msg = "Train brain ainainain test ainainain"
+output = tok.train(msg, 260)
 print(msg)
-print(len(msg))
 print(output)
-print(len(output))
+print(tok.merges)
